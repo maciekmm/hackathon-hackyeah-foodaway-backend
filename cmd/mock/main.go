c71f9d571f9e234f9538ff21d678bb8097b7cc19
Maciej Mionskowski <business@maciekmm.net>
2017-10-28 23:13:00 +0200

    Added mock data generator

---
+++ cmd/mock/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/maciekmm/HackYeah/app"
+	"github.com/maciekmm/HackYeah/model"
+)
+
+const (
+	CracowLatitude  = 50.06143
+	CracowLongitude = 19.93658
+	//MaxDistance in degrees
+	MaxDistance = 0.3
+)
+
+var (
+	food = []string{"Artichoke", "Arugula", "Asparagus", "Green, Purple, White", "Avocado", "Bamboo Shoots", "Bean Sprouts", "Beans- see Bean List", "Beet", "Belgian Endive", "Bell Pepper", "Bitter Melon/Bitter Gourd", "Bok Choy/Bok Choi/Pak Choy", "Broccoli", "Brussels Sprouts", "Burdock Root/Gobo", "Cabbage", "Green, Red, Savoy", "Calabash", "Capers", "Carrot", "Cassava/Yuca", "Cauliflower", "Celery", "Celery Root/Celeriac", "Celtuce", "Chayote", "Chinese Broccoli/Kai-lan", "Corn/Maize", "Baby Corn/Candle Corn", "Cucumber", "English Cucumber", "Gherkin", "Pickling Cucumbers", "Daikon Radish", "Edamame", "Eggplant/Aubergine", "Elephant Garlic", "Endive", "Curly/Frisee", "Escarole", "Fennel", "Fiddlehead", "Galangal", "Garlic", "Ginger", "Grape Leaves", "Green Beans/String Beans/Snap Beans", "Wax Beans", "Greens", "Amaranth Leaves/Chinese Spinach", "Beet Greens", "Collard Greens", "Dandelion Greens", "Kale", "Kohlrabi Greens", "Mustard Greens", "Rapini", "Spinach", "Swiss Chard", "Turnip Greens", "Hearts of Palm", "Horseradish", "Jerusalem Artichoke/Sunchokes", "Jícama", "Kale", "Curly", "Lacinato", "Ornamental", "Kohlrabi", "Leeks", "Lemongrass", "Lettuce", "Butterhead- Bibb, Boston", "Iceberg", "Leaf- Green Leaf, Red Leaf", "Romaine", "Lotus Root", "Lotus Seed", "Mushrooms- see Mushroom List", "Napa Cabbage", "Nopales", "Okra", "Olive", "Onion", "Green Onions/Scallions", "Parsley", "Parsley Root", "Parsnip", "Peas", "green peas", "snow peas", "sugar snap peas", "Peppers- see Peppers List", "Plantain", "Potato", "Pumpkin", "Purslane", "Radicchio", "Radish", "Rutabaga", "Sea Vegetables- see Sea Vegetable List", "Shallots", "Spinach", "Squash- see Squash List", "Sweet Potato", "Swiss Chard", "Taro", "Tomatillo", "Tomato", "Turnip", "Water Chestnut", "Water Spinach", "Watercress", "Winter Melon", "Yams", "Zucchini"}
+)
+
+func nextSign() float64 {
+	r := rand.Intn(1)
+	if r > 0 {
+		return -1
+	}
+	return 1
+}
+
+func init() {
+	rand.Seed(time.Now().Unix())
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Invalid number of arguments.")
+		os.Exit(1)
+	}
+	number, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Println("First argument should be a count")
+		os.Exit(1)
+	}
+
+	logger := log.New(os.Stdout, "HackYeah!", log.Ldate|log.Lshortfile)
+	app := &app.Application{Logger: logger}
+
+	err = app.Init()
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	for ; number > 0; number-- {
+		latitude := CracowLatitude + nextSign()*MaxDistance*rand.Float64()
+		longitude := CracowLongitude + nextSign()*MaxDistance*rand.Float64()
+		offer := model.Offer{
+			Latitude:    &latitude,
+			Longitude:   &longitude,
+			Title:       food[rand.Intn(len(food))],
+			Expiration:  uint64(time.Now().Unix() + int64(rand.Intn(1000*60*60*24*7))),
+			PickupStart: uint64(time.Now().Unix()),
+			PickupEnd:   uint64(time.Now().Unix() + int64(rand.Intn(1000*60*60*24*7))),
+			Description: "test",
+		}
+		if err := offer.Add(app.Database); err != nil {
+			logger.Fatal(err)
+		}
+	}
+}
--- main.go
+++ main.go
@@ -1,109 +1,24 @@
 package main
 
 import (
-	"context"
-	"database/sql"
-	"fmt"
 	"log"
 
 	"os"
 
-	"net/http"
-
-	"time"
-
-	"github.com/gorilla/mux"
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/postgres"
-	_ "github.com/lib/pq"
-	"github.com/maciekmm/HackYeah/controller"
-	"github.com/maciekmm/HackYeah/model"
+	"github.com/maciekmm/HackYeah/app"
 )
 
 func main() {
 	logger := log.New(os.Stdout, "HackYeah!", log.Ldate|log.Lshortfile)
-	app := &Application{Logger: logger}
+	app := &app.Application{Logger: logger}
 
-	err := app.init()
+	err := app.Init()
 	if err != nil {
 		logger.Fatal(err)
 	}
 
-	err = app.serve()
+	err = app.Serve()
 	if err != nil {
 		logger.Fatal(err)
 	}
-}
-
-type Application struct {
-	Database *gorm.DB
-	Logger   *log.Logger
-	router   *mux.Router
-}
-
-func (a *Application) init() error {
-	a.Logger.Println("starting HackYeah")
-
-	a.Logger.Println("setting up database connection")
-	con, err := sql.Open("postgres", fmt.Sprintf("postgres://%s:%s@database/%s?sslmode=disable", os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_DB")))
-	if err != nil {
-		return fmt.Errorf("could not open database connection: %s", err.Error())
-	}
-
-	a.Logger.Println("establishing database connection")
-	deadline := time.After(10 * time.Second)
-out:
-	for {
-		select {
-		case <-deadline:
-			return fmt.Errorf("could not establish database connection, last error: %s", err.Error())
-		default:
-			ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
-			defer cancel()
-			err = con.PingContext(ctx)
-			if err == nil {
-				break out
-			} else {
-				a.Logger.Printf("pinging database failed: %s\n", err.Error())
-			}
-			time.Sleep(1 * time.Second)
-		}
-	}
-	// auto-migrating model
-	a.Database, err = gorm.Open("postgres", con)
-	a.Database.SetLogger(a.Logger)
-	if err != nil {
-		return err
-	}
-
-	a.Database.AutoMigrate(&model.User{}, &model.Offer{})
-
-	// setup routes
-	a.Logger.Println("setting up routes")
-	a.router = mux.NewRouter()
-
-	// shitty hack to counter browser CORS clients, it's hackathon all tricks are allowed
-	a.router.Methods("OPTIONS").HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		rw.WriteHeader(http.StatusOK)
-	})
-
-	// accounts
-	accountController := &controller.Accounts{Database: a.Database}
-	accountController.Register(a.router.PathPrefix("/accounts/").Subrouter())
-
-	// offers
-	offerController := &controller.Offers{Database: a.Database}
-	offerController.Register(a.router.PathPrefix("/offers/").Subrouter())
-	return nil
-}
-
-func (a *Application) serve() error {
-	server := http.Server{
-		Addr:           ":3000",
-		Handler:        a.router,
-		ReadTimeout:    15 * time.Second,
-		WriteTimeout:   15 * time.Second,
-		MaxHeaderBytes: 1 << 20,
-	}
-	return server.ListenAndServe()
 }
